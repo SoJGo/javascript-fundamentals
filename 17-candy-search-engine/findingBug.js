9e7b6a6038231836ee9ccca1aaec592fd58d7da4
SophieGowen <sophie.j.gowen@gmail.com>
2022-01-27 14:20:36 +0000

    Finish 17

---
+++ 17-candy-search-engine/findingBug.js
@@ -0,0 +1,21 @@
+// // original
+
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const isEvenNumber = (number) => {
+//   number % 2 == 0;
+// }
+
+// const evenNumbers = filter(isEvenNumber(numbers));
+
+// after
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const isEvenNumber = (number) => {
+  return number % 2 == 0;
+}
+
+const evenNumbers = numbers.filter(isEvenNumber);
+
+console.log(evenNumbers);
